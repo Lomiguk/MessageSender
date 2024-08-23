39fba23b9ab5c828a3b8f7da8d300f40faa7be1d
Denis Skibin <d.sckibin2017@yandex.ru>
2024-08-23 11:44:22 +0300

    Add message controller

--- src/main/java/ru/sckibin/messagesender/api/enumType/MessageType.java
+++ src/main/java/ru/sckibin/messagesender/api/enumType/MessageType.java
@@ -1,2 +1,12 @@
-package ru.sckibin.messagesender.api.enumType;public enum MessageType {
+package ru.sckibin.messagesender.api.enumType;
+
+public enum MessageType {
+
+    EMAIL ("email");
+
+    public final String value;
+
+    MessageType(String value) {
+        this.value = value;
+    }
 }
