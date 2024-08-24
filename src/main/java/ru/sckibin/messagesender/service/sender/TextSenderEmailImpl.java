7b3b2157fc127d7d080475b26bd50cb4f20c60aa
Denis Skibin <d.sckibin2017@yandex.ru>
2024-08-24 11:25:41 +0300

    16 Fix async - Set saving logic to the async method

--- src/main/java/ru/sckibin/messagesender/service/sender/TextSenderEmailImpl.java
+++ src/main/java/ru/sckibin/messagesender/service/sender/TextSenderEmailImpl.java
@@ -7,19 +7,33 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import ru.sckibin.messagesender.api.dto.MessageDTO;
+import ru.sckibin.messagesender.api.enumType.MessageStatus;
+import ru.sckibin.messagesender.repository.MessageRepository;
+import ru.sckibin.messagesender.util.MessageUtil;
 
-@Service
+@Service("emailSender")
 @RequiredArgsConstructor
 public class TextSenderEmailImpl implements TextSender {
 
     public final JavaMailSender mailSender;
+    public final MessageRepository messageRepository;
+    public final MessageUtil messageUtil;
+
     @Value("${spring.mail.sender}")
     private String senderEmailAddress;
 
     @Async
     @Override
     public void sendTextMessage(MessageDTO message) {
-        sendTextMessage(message.getRecipient(), message.getSubject(), message.getBody());
+        try {
+            sendTextMessage(message.getRecipient(), message.getSubject(), message.getBody());
+            message.setStatus(MessageStatus.SENT);
+        } catch (Exception ex) {
+            message.setStatus(MessageStatus.FAILED);
+            message.setFailedDescription(ex.getMessage());
+        }
+
+        messageRepository.save(messageUtil.mapToEntity(message));
     }
 
     private void sendTextMessage(String recipient, String subject, String body) {
